942face1bdf7f2b821fc749262254a484eae15d6
Moss <maylesa06@outlook.com>
2024-03-22 20:13:59 -0400

    Following a tutorial for rendering so I understand

---
+++ engine.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <thread>
+
+#include <
+
+using namespace std;
+using namespace std::this_thread;
+using namespace std::chrono;
+
+int main (int argc, char *argv[]) {
+
+}
